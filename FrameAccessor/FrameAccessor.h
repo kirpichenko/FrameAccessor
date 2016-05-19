bd4b8d53273031e26279a7cb7ea5426b401102d5
Evgeniy Kirpichenko <evgeniy.kirpichenko@talendo.ch>
2016-05-19 12:20:12 +0200

    Add cartage support

--- FrameAccessor/FrameAccessor.h
+++ FrameAccessor/FrameAccessor.h
@@ -2,12 +2,23 @@
 //  FrameAccessor.h
 //  FrameAccessor
 //
-//  Created by Alex Denisov on 18.03.12.
-//  Copyright (c) 2013 okolodev.org. All rights reserved.
+//  Created by Evgeniy Kirpichenko on 19/05/16.
+//  Copyright © 2016 Talendo. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameAccessor.
+FOUNDATION_EXPORT double FrameAccessorVersionNumber;
+
+//! Project version string for FrameAccessor.
+FOUNDATION_EXPORT const unsigned char FrameAccessorVersionString[];
 
 #import <FrameAccessor/ViewFrameAccessor.h>
 
 #if (TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE)
-    #import <FrameAccessor/ScrollViewFrameAccessor.h>
+#import <FrameAccessor/ScrollViewFrameAccessor.h>
 #endif
+
+
+
